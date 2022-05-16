a644681e4c93abea10c1c4080dc49db8f9bea6b8
RedRaptor10 <weiminh_lam@yahoo.com>
2022-05-16 12:26:04 -0500

    Update Footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <footer>
-            <span>My Trakt is powered by <a href="https://trakt.tv/" target="_blank" rel="noreferrer">Trakt</a> using the official Trakt API,
-            and <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">The Movie Database</a> using the TMDB API.</span>
+            <span>My Trakt is powered by <a href="https://trakt.tv/" target="_blank" rel="noreferrer">Trakt</a> and <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">The Movie Database</a> using
+            the official Trakt API and TMDB API.</span>
         </footer>
     )
 };
