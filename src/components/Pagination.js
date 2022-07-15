fee9fc46e67ff22d0206b25e6212e5e313a8bee9
RedRaptor10 <weiminh_lam@yahoo.com>
2022-07-14 19:19:37 -0500

    Refactor pagination code from JSX to JS

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,34 +1,36 @@
 const Pagination = ({ results, type, page, setPage, limit, view, changeView }) => {
-    const totalPages = Math.ceil(results.length / limit);
+    const totalPages = results.length > 0 ? Math.ceil(results.length / limit) : 1;
+    let pages = [];
+
+    if (totalPages <= 9) {
+        for (let i = 1; i <= totalPages; i++) {
+            pages.push(i);
+        }
+    } else if (page <= 5) {
+        for (let i = 1; i <= 9; i++) {
+            pages.push(i);
+        }
+    } else if (page > totalPages - 4) {
+        for (let i = totalPages - 8; i <= totalPages; i++) {
+            pages.push(i);
+        }
+    } else {
+        for (let i = page - 4; i <= page + 4; i++) {
+            pages.push(i);
+        }
+    }
 
     return (
     <div className="pagination">
         <div className="pagination-btns">
             {page > 1 ? <div className="pagination-nav-btn" onClick={() => { setPage(1) }}>First</div> : null}
             {page > 1 ? <div className="pagination-nav-btn" onClick={() => { setPage(page - 1) }}>Prev</div> : null}
-            { // Create an array of size 9
-            [...Array(9)].map((p, i) => {
-            return (
-                // Render number if current page is within first 4 pages and less than or equal to total pages
-                page <= 4 && i + 1 <= totalPages ?
-                <div key={i + 1} className={i + 1 !== page ? 'pagination-btn' : 'pagination-current-btn'} onClick={
-                    i + 1 !== page ? () => { setPage(i + 1) } : null
-                }>{i + 1}</div>
-                :
-                // Render number if current page is within last 4 pages and there are at least 9 pages
-                page > totalPages - 4 && totalPages > 8 ?
-                <div key={i + totalPages - 8} className={
-                    i + totalPages - 8 !== page ? 'pagination-btn' : 'pagination-current-btn'} onClick={
-                    i + totalPages - 8 !== page ? () => { setPage(i + totalPages - 8) } : null
-                }>{i + totalPages - 8}</div>
-                :
-                // Render previous 4 numbers and remaining numbers if there are at least 4 pages
-                page > 4 ?
-                    <div key={i + page - 4} className={i + page - 4 !== page ? 'pagination-btn' : 'pagination-current-btn'} onClick={
-                        i + page - 4 !== page ? () => { setPage(i + page - 4) } : null
-                    }>{i + page - 4}</div>
-                : null
-            )
+            {pages.map(p => {
+                return (
+                    <div key={p} className={p !== page ? 'pagination-btn' : 'pagination-current-btn'} onClick={
+                        p !== page ? () => { setPage(p) } : null
+                    }>{p}</div>
+                );
             })}
             {page < totalPages ? <div className="pagination-nav-btn" onClick={() => { setPage(page + 1) }}>Next</div> : null}
             {page < totalPages ? <div className="pagination-nav-btn" onClick={() => { setPage(totalPages) }}>Last</div> : null}
