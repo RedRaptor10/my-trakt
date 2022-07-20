34091b770e1ffb08aefd263c681cb5af2c1defcd
RedRaptor10 <weiminh_lam@yahoo.com>
2022-07-20 04:04:19 -0500

    Implement user profile fetching. Add User route.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
+import User from './User';
 import Items from './Items';
 import Lists from './Lists';
 
@@ -17,17 +18,21 @@ const api_key = process.env.REACT_APP_API_KEY; // TMDB auth
 
 const App = () => {
     const [loading, setLoading] = useState(false);
+    const [user, setUser] = useState();
     const [collection, setCollection] = useState();
     const [lists, setLists] = useState();
     const [list, setList] = useState();
+    const [errorMsg, setErrorMsg] = useState();
 
     // Note: A limitation to the Trakt API is it cannot return a subset of results.
     // We must fetch the entire collection and set it as a state.
-    const fetchData = useCallback(async (resource, type, listId) => {
+    const fetchData = useCallback(async (userId, resource, type, listId) => {
         setLoading(true);
 
         let protocol = '';
-        if (resource === 'collection' && type) {
+        if (resource === 'profile') {
+            protocol = '/users/' + userId;
+        } else if (resource === 'collection' && type) {
             protocol = '/users/' + trakt_id + '/collection/' + type;
         } else if (resource === 'lists') {
             protocol = '/users/' + trakt_id + '/lists';
@@ -48,7 +53,11 @@ const App = () => {
     
         // Return fetch results as Promise
         const data = await fetch(trakt_url + protocol, options);
-        return data.json();
+        if (data.status === 404) {
+            return false;
+        } else {
+            return data.json();
+        }
     }, [setLoading])
 
     const fetchPosters = useCallback(async (r, t) => {
@@ -103,15 +112,16 @@ const App = () => {
 
     return (
         <HashRouter>
-            <Header />
+            <Header setLoading={setLoading} setUser={setUser} setErrorMsg={setErrorMsg} fetchData={fetchData} />
             <Routes>
-                <Route exact path="/" element={<Home loading={loading} setLoading={setLoading} list={list} setList={setList}
-                    fetchData={fetchData} fetchPosters={fetchPosters} />} />
+                <Route exact path="/" element={<Home errorMsg={errorMsg} />} />
+                <Route exact path="/:username" element={<User loading={loading} setLoading={setLoading} user={user} setUser={setUser} errorMsg={errorMsg}
+                    setErrorMsg={setErrorMsg} fetchData={fetchData} />} />
                 <Route exact path="/movies" element={<Items loading={loading} setLoading={setLoading} collection={collection} setCollection={setCollection}
                     list={list} setList={setList} typeProp={'movies'} fetchData={fetchData} fetchPosters={fetchPosters} />} />
                 <Route exact path="/shows" element={<Items loading={loading} setLoading={setLoading} collection={collection} setCollection={setCollection}
                     list={list} setList={setList} typeProp={'shows'} fetchData={fetchData} fetchPosters={fetchPosters} />} />
-                <Route exact path="/lists" element={<Lists loading={loading} setLoading={setLoading} lists={lists} setLists={setLists}
+                <Route exact path="/:username/lists" element={<Lists loading={loading} setLoading={setLoading} lists={lists} setLists={setLists}
                     fetchData={fetchData} />} />
                 <Route exact path="/lists/:listId" element={<Items loading={loading} setLoading={setLoading} collection={collection} setCollection={setCollection}
                     list={list} setList={setList} typeProp={'list'} fetchData={fetchData} fetchPosters={fetchPosters} />} />
