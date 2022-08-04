deea959664188c9427d3481d5bb64930aa2548df
RedRaptor10 <weiminh_lam@yahoo.com>
2022-08-04 15:29:00 -0500

    Add logo and profile button to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
+import logo from '../assets/trakt-icon-red.svg';
 
 const Home = () => {
     return (
         <main className="home">
-            <div>Welcome to My Trakt. Enter a Trakt username or id in the search bar to view a user's profile.</div>
-            <Link to="/RedRaptor10"><button>Example Profile</button></Link>
+            <img src={logo} alt="" />
+            <div>
+                <b>My Trakt</b> is a web app that displays a user's favorite movies and shows.<br />
+                Enter a Trakt username or id in the search bar to view a user's profile.
+            </div>
+            <Link to="/redraptor10"><button>PROFILE EXAMPLE</button></Link>
         </main>
     );
 };
